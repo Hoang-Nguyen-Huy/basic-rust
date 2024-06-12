87318ec6579f735c42242d4611ce71e51aa19fc2
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-06-13 00:30:46 +0700

    init

---
+++ hello-world/target/debug/deps/hello_world-786aa1297855be45.d
@@ -0,0 +1,5 @@
+/Users/hoangnguyen/Documents/BlockChain/basic-rust/hello-world/target/debug/deps/libhello_world-786aa1297855be45.rmeta: src/main.rs
+
+/Users/hoangnguyen/Documents/BlockChain/basic-rust/hello-world/target/debug/deps/hello_world-786aa1297855be45.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/deps/hello_world-d42a0a5607c5261d.d
@@ -0,0 +1,5 @@
+/Users/hoangnguyen/Documents/BlockChain/basic-rust/hello-world/target/debug/deps/libhello_world-d42a0a5607c5261d.rmeta: src/main.rs
+
+/Users/hoangnguyen/Documents/BlockChain/basic-rust/hello-world/target/debug/deps/hello_world-d42a0a5607c5261d.d: src/main.rs
+
+src/main.rs:
